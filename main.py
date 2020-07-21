025b9c2b537f259e9c0923f8ff7cdc2fbc6aa159
NicolleLouis <louisxnicolle@gmail.com>
2020-07-21 12:17:46 +0200

    Insert all first line in db

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
+import time
+
 from service.sqlite_service import SQLiteService
 from service.files_service import FileService
+from service.file_reader_service import FileReaderService
 
 from db_query import sqlite_create_table_query, sqlite_print_table, sqlite_drop_table
 
-sqliteConnection, cursor = SQLiteService.open_sqlite()
-# Create table
-cursor.execute(sqlite_create_table_query)
+# Time analysis
+start_time = time.time()
 
-#Insert aujourdhui file in table
+# Create table
+sqliteConnection, cursor = SQLiteService.open_sqlite()
+try:
+    cursor.execute(sqlite_create_table_query)
+except Exception as e:
+    print("<----- Error: {} ------>".format(e))
+    pass
+
+# Insert aujourdhui file in table
 file = open(FileService.get_aujourdhui_file_path(), 'r')
-lines = file.readlines()
-print(lines[1])
+lines = file.readlines()[1:]
+for line in lines:
+    try:
+        FileReaderService.insert_line_in_db(
+            sqliteConnection=sqliteConnection,
+            cursor=cursor,
+            line=line
+        )
+    except Exception as e:
+        print(line)
+        print(e)
 
 # Look at table
 cursor.execute(sqlite_print_table)
 
 # Delete table for next execution
-cursor.execute(sqlite_drop_table)
+# todo remove this a the end of the project
+# cursor.execute(sqlite_drop_table)
 SQLiteService.close_sqlite(sqliteConnection, cursor)
+
+# Time analysis
+print("--- %s seconds ---" % (time.time() - start_time))
--- service/file_reader_service.py
+++ service/file_reader_service.py
@@ -1,4 +1,15 @@
-class FileReader:
+from db_query import sqlite_insert_line
+
+
+class FileReaderService:
     @staticmethod
     def read_line(line):
-        print(line)
+        values = line.split("|")
+        values = list(map(lambda value: "\"{}\"".format(value) if value != "" else "NULL", values))
+        return values
+
+    @staticmethod
+    def insert_line_in_db(sqliteConnection, cursor, line):
+        sqlite_insert = sqlite_insert_line.format(*FileReaderService.read_line(line))
+        cursor.execute(sqlite_insert)
+        sqliteConnection.commit()
